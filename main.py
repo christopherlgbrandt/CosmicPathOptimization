54a24265c40e929b1f560f31a77393ea2f83b0c8
Christopher Brandt <christopherlgbrandt@gmail.com>
2023-10-29 21:25:53 -0600

    TestingCosmicOptimizationProblem

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 # Christopher Brandt
 import sys
 
-numReadings=input()
-numReadings=int(numReadings)
-averageReading=0
-reading=input()
-tempReadings=reading.split()
-if (numReadings!=len(tempReadings)):
+try:
+    numReadings = input()
+    numReadings =  numReadings.strip()
+    
+    if numReadings.isdigit():
+        numReadings = int(numReadings)
+    else:
+        sys.exit()
+except ValueError:
+    sys.exit()
+
+averageReading = 0
+reading = input()
+tempReadings = reading.split()
+if (numReadings != len(tempReadings)):
     sys.exit()
 
 for reading in tempReadings:
